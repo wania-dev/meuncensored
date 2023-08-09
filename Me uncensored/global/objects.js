5c39e67c896a4568e79e38f5056c2273c97e48ca
Wania <107751144+wania-dev@users.noreply.github.com>
2023-08-10 00:17:23 +0500

    Update objects.js

--- Me uncensored/global/objects.js
+++ Me uncensored/global/objects.js
@@ -4,7 +4,7 @@ heading: "Unboxing Barbie — A Personal Reflection",
 description: "What about the static nature of Barbie makes her the perfect woman? What makes perfectionism so enticing? What does the pursuit of perfectionism entail?",
 date: "2023-08-01",
 coverImage: "https://assets.teenvogue.com/photos/6449533733b9199ea36a5973/16:9/w_2240,c_limit/rev-1-BARBIE-TP-0002_High_Res_JPEG.jpeg",
-filePath: "Unboxing-barbie-a-personal-reflection.htm",
+filePath: "Unboxing-barbie-a-personal-reflection.html",
 category: ["CULTURE & HERITAGE"],
 }
     ,
